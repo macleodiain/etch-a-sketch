bdf9296f6d932581dfcb75441bc659320dde8802
Iain Macleod <macleod@live.co.uk>
2023-06-12 02:59:48 +0100

    Grid now draws at 16*16 buttton added to change grid size validation added to user input

--- easScript.js
+++ easScript.js
@@ -1,12 +1,56 @@
-let size = Number(window.prompt("Please enter a number:"))
-console.log(`${size}`)
+
+let size = 16
+
+
+function getGridSize(){
+size = Number(window.prompt("Please enter a whole number between 1 and 100:"))
+
 while((isNaN(size))||(!Number.isInteger(size))){
     size = Number(window.prompt("Invalid input.  Please enter a number:"))
 }
-console.log(`${typeof(size)}`)
 
+while((size>100)||(size<1)){
+    size = Number(window.prompt("Invalid input.  Number must be between 1-100:"))
+    while((isNaN(size))||(!Number.isInteger(size))){
+        size = Number(window.prompt("Invalid input.  Please enter a number:"))
+    }
+}
+main.innerHTML = ""
+createGrid(size)
+}
+
+const changeButton = document.createElement('button')
+changeButton.textContent = "Change grid size:"
+changeButton.addEventListener('click', () => {
+    getGridSize()
+})
 const main = document.createElement('div')
+
 main.classList.add('main')
-main.textContent = "main container"
+
+document.body.appendChild(changeButton)
 document.body.appendChild(main)
+
+
+function createGrid(width){
+    
+for(let i = 0; i<size; i++){
+    let row = document.createElement('div')
+    row.classList.add("row")
+        for(let i =0; i< size;i++){
+
+        let x = document.createElement('div')
+        x.classList.add("cell")
+        x.addEventListener("mouseover", () =>{
+            x.classList.add("hover")
+        })
+        row.appendChild(x)
+    }
+    main.appendChild(row)
+}
+
+}
+
+createGrid(size)
+
 
