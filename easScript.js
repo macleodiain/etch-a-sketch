83e0186cdb8c0a5f08d55e1dbf4c903ac0a90661
Iain Macleod <macleod@live.co.uk>
2023-06-12 01:25:03 +0100

    Creates main container

--- easScript.js
+++ easScript.js
@@ -4,3 +4,9 @@ while((isNaN(size))||(!Number.isInteger(size))){
     size = Number(window.prompt("Invalid input.  Please enter a number:"))
 }
 console.log(`${typeof(size)}`)
+
+const main = document.createElement('div')
+main.classList.add('main')
+main.textContent = "main container"
+document.body.appendChild(main)
+
